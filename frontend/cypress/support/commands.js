15d961e270ab2f83dc9d49345431bd12f77d60bb
futurepear <futurethepear@gmail.com>
2026-05-30 13:59:11 -0700

    frotnedn cyrpress bs

---
+++ frontend/cypress/support/commands.js
@@ -0,0 +1,7 @@
+// Place custom Cypress commands here.
+// Example:
+// Cypress.Commands.add('login', (username, password) => {
+//   cy.get('[data-test=username]').type(username);
+//   cy.get('[data-test=password]').type(password);
+//   cy.get('[data-test=login]').click();
+// });
